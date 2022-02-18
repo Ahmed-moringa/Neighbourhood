8c8bcf8754f39827fdbb15c94f94eec2173bbacb
Ahmed-moringa <ahmed.mohamed@student.moringaschool.com>
2022-02-18 04:48:18 +0300

    created multiple models

--- neighbour/models.py
+++ neighbour/models.py
@@ -1,35 +1,75 @@
-from re import T
-from unicodedata import name
-from django.db import models
-
-# Create your models here.
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
-class Neighbourhood(models.Model):
-    name = models.CharField(max_length=60, blank=True)
-    location = models.CharField(max_length=60, blank=True)
-    occupants = models.CharField(max_length=10, blank=True)
-    # admin = models.ForeignKey(Admin, on_delete=models.CASCADE, related_name='neighbourhood' )
-    
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    occupants = models.IntegerField(null=True, blank=True)
+
     def __str__(self):
-        return self.name()
+        return self.name
 
-class User(models.Model):
-     name = models.CharField(max_length=60, blank=True)
-     id = models.CharField(max_length=10, blank=True,primary_key=True)
-     neighbour_identificaion = models.ForeignKey(User, on_delete=models.CASCADE, related_name='neighbourhood_id')
-     email = models.EmailField()
+    def create_neighborhood(self):
+        self.save()
 
-     def __str__(self):
-        return self.name()
+    def delete_neighborhood(self):
+        self.delete()
 
-class Buisness(models.Model):
-    name = models.CharField(max_length=60, blank=True)
-    email = models.EmailField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='neighbourhood' )
-    neighbour_identification = models.ForeignKey(User, on_delete=models.CASCADE, related_name='neighbourhood_id')
-    
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = models.ImageField(upload_to='images/')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+
     def __str__(self):
-        return self.name()
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.EmailField(max_length=254)
+    description = models.TextField(blank=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='business')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='owner')
+
+    def __str__(self):
+        return f'{self.name} Business'
+
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(name__icontains=name).all()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=120, null=True)
+    post = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='post_owner')
+    hood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='hood_post')
+
